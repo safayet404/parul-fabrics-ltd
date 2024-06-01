45609b01c491b223e3b4fd7bf84c77e20afecf08
Safayet <hossainsafayet187@gmail.com>
2024-06-01 21:47:01 +0600

    cors : backend link correction

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
 // export const base_url = "http://localhost:7000/api/";
-export const base_url = "https://parul-fabrics-ltd-backend.onrender.com/api/";
+export const base_url = "https://parul-fabrics-ltd-backend.vercel.app/api/";
